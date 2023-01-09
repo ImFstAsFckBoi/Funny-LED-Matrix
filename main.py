bc010c93878c783e53384201e70d2f87282574bb
ImFstAsFckBoi <m.olivesten@protonmail.com>
2023-01-10 00:04:49 +0100

    testing escape sequences

--- main.py
+++ main.py
@@ -6,6 +6,43 @@ from time import sleep
 
 ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ"
 
+# Escape Sequence Renaming Table
+ESRT: dict[int, str] = {}
+PUA_LOWER = 0xE000
+PUA_UPPER = 0xF8FF
+
+# Uses only numbers in Unicode's 'Private Use Areas'
+# https://en.wikipedia.org/wiki/Private_Use_Areas#Assignment
+
+ESRT_COUNTER = PUA_LOWER
+
+
+def replace_escape_sequence(msg: str, idx: int):
+    global ESRT_COUNTER
+    sequence = ''
+
+    for i in msg[idx:]:
+        if i == '\\':
+            break
+
+        sequence += i
+    else:
+        raise Exception(f'Escape sequence at index {idx} is never closed.')
+
+    msg = msg.replace(f'\\{sequence}\\', chr(ESRT_COUNTER))
+    ESRT[ESRT_COUNTER] = sequence
+    ESRT_COUNTER += 1
+
+
+def preprocess_escape_sequences(msg: str):
+    while True:
+        for idx in range(len(msg)):
+            if msg[idx] == '\\':
+                replace_escape_sequence(msg, idx)
+                break
+        else:
+            break
+
 
 def msg_slice(msg: str, n: int) -> tuple[str, int]:
     idx = 0
@@ -18,10 +55,16 @@ def msg_slice(msg: str, n: int) -> tuple[str, int]:
 
 def main(conn: max7219cng):
     msg = f' {input("Your Message: ")} '
+    preprocess_escape_sequences(msg)
     for msg_line in range(6 * (len(msg) - 1)):
         for matrix_line in range(1, 8):
-            a = msg_slice(msg, msg_line + matrix_line)
-            conn.write(get_symbol_line(a[0], a[1], matrix_line))
+            c_l_tuple = msg_slice(msg, msg_line + matrix_line)
+
+            if ord(c_l_tuple[0]) >= PUA_LOWER or ord(c_l_tuple[0]) <= PUA_UPPER:  # noqa
+                char = f'__{ESRT[ord(c_l_tuple[0])]}__'
+            else:
+                char = c_l_tuple[0]
+            conn.write(get_symbol_line(char, c_l_tuple[1], matrix_line))
         sleep(0.2)
 
 
