d561ac1bc1a2b571511724f1681d395e3d14389b
ImFstAsFckBoi <m.olivesten@protonmail.com>
2023-02-12 01:30:05 +0100

    Refactored matrix functions to lib.py

---
+++ lib.py
@@ -0,0 +1,133 @@
+"""Library for printing scrolling text to a LED matrix
+"""
+
+from drivers.max7219cng import max7219cng
+from font import get_symbol_line, get_symbol
+from time import sleep
+
+# Escape Sequence Renaming Table
+# Uses only numbers in Unicode's 'Private Use Areas'
+# https://en.wikipedia.org/wiki/Private_Use_Areas#Assignment
+__ESRT: dict[int, str] = {}
+__PUA_LOWER = 0xE000
+__PUA_UPPER = 0xF8FF
+__ESRT_COUNTER = __PUA_LOWER
+
+
+class EscapeError(Exception):
+    ...
+
+
+def __replace_escape_sequence(msg: str, idx: int) -> str:
+    global __ESRT_COUNTER
+    rev_sequence = ''
+
+    for i in reversed(msg[:idx]):
+        if i == '\\':
+            break
+
+        rev_sequence += i
+    else:
+        raise EscapeError(f'Escape sequence at index {idx} is never closed.')
+
+    if len(rev_sequence) == 0:
+        return msg[:idx] + msg[idx + 1:]
+
+    sequence = rev_sequence[::-1]
+
+    msg = msg.replace(f'\\{sequence}\\', chr(__ESRT_COUNTER))
+    __ESRT[__ESRT_COUNTER] = sequence
+    __ESRT_COUNTER += 1
+    return msg
+
+
+def __preprocess_escape_sequences(msg: str) -> str:
+    while True:
+        idx = len(msg) - 1
+
+        while idx >= 0:
+            if msg[idx] == '\\':
+                msg = __replace_escape_sequence(msg, idx)
+                if idx < len(msg) and msg[idx - 1] == '\\':
+                    idx -= 1
+                else:
+                    break
+            idx -= 1
+        else:
+            break
+
+    return msg
+
+
+def __escape_char(char: str) -> str:
+    if ord(char) >= __PUA_LOWER and ord(char) <= __PUA_UPPER:  # noqa
+        return f'__{__ESRT[ord(char)]}__'
+
+    return char
+
+
+def __remove_rename(code: int) -> None:
+    global __ESRT_COUNTER
+    if code != __ESRT_COUNTER:
+        raise EscapeError('Escape character was handled incorrectly')
+
+    __ESRT_COUNTER -= 1
+
+
+def __preprocess_slice_message(msg: str) -> list[tuple[str, int]]:
+    """Convert a string into a sequence of tuples containing
+    (<character>, <line of character>) to be used as print instruction.
+
+    Args:
+        `msg` (str): String to convert.
+    Returns:
+        list[tuple[str, int]]: Sequence of print instructions.
+    """
+    msg_layout: list[tuple[str, int]] = []
+
+    for char in msg:
+        for idx in range(1, get_symbol(__escape_char(char)).width + 1):
+            msg_layout.append((char, idx))
+
+        msg_layout.append((' ', 1))
+
+    return msg_layout
+
+
+def print2matrix(msg: str, matrix: max7219cng,
+                 sleep_time: float = 0.1,
+                 lpadding: int = 3,
+                 rpadding: int = 3):
+    """Print scrolling text to max7219cng LED matrix.
+
+    Args:
+        `msg` (str): String to print.
+        `matrix` (max7219cng): Driver object to print to.
+        `sleep_time` (float, optional): Sleeping time between instructions,
+        Lower = Faster. Defaults to 0.1.
+        `lpadding` (int, optional): Space padding before message,
+        Defaults to 3.
+        `rpadding` (int, optional): Space padding after message,
+        Defaults to 3.
+    """
+
+    msg = f'{" " * lpadding}{msg}{" " * rpadding}'
+
+    try:
+        msg = __preprocess_escape_sequences(msg)
+    except EscapeError:
+        print('Incorrectly formatted sequence')
+        return
+
+    layout = __preprocess_slice_message(msg)
+
+    for msg_line in range(len(layout) - 8):
+        for matrix_line in range(8):
+            char, line = layout[msg_line + matrix_line]
+
+            char = __escape_char(char)
+            matrix.write(get_symbol_line(char, line, matrix_line + 1))
+        sleep(sleep_time)
+
+    for i in range(__ESRT_COUNTER - 1, __PUA_LOWER, -1):
+        __remove_rename(i)
--- main.py
+++ main.py
@@ -1,119 +1,31 @@
 #!/usr/bin/env python3
 
 from drivers.max7219cng import max7219cng
-from font import get_symbol_line, get_symbol
-from time import sleep
-
-ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ"
-
-# Escape Sequence Renaming Table
-ESRT: dict[int, str] = {}
-PUA_LOWER = 0xE000
-PUA_UPPER = 0xF8FF
-
-# Uses only numbers in Unicode's 'Private Use Areas'
-# https://en.wikipedia.org/wiki/Private_Use_Areas#Assignment
-
-ESRT_COUNTER = PUA_LOWER
-
-
-def replace_escape_sequence(msg: str, idx: int) -> str:
-    global ESRT_COUNTER
-    rev_sequence = ''
-
-    for i in reversed(msg[:idx]):
-        if i == '\\':
-            break
-
-        rev_sequence += i
-    else:
-        raise Exception(f'Escape rev_sequence at index {idx} is never closed.')
-
-    if len(rev_sequence) == 0:
-        return '\\'
-
-    sequence = rev_sequence[::-1]
-
-    msg = msg.replace(f'\\{sequence}\\', chr(ESRT_COUNTER))
-    ESRT[ESRT_COUNTER] = sequence
-    ESRT_COUNTER += 1
-    return msg
-
-
-def preprocess_escape_sequences(msg: str) -> str:
-    while True:
-        for idx in range(len(msg) - 1, 0, -1):
-            if msg[idx] == '\\':
-                msg = replace_escape_sequence(msg, idx)
-                break
-        else:
-            break
-
-    return msg
-
-
-def escape_char(char: str) -> str:
-    if ord(char) >= PUA_LOWER and ord(char) <= PUA_UPPER:  # noqa
-        return f'__{ESRT[ord(char)]}__'
-
-    return char
-
-
-def remove_rename(code: int) -> None:
-    global ESRT_COUNTER
-    if code != ESRT_COUNTER:
-        raise Exception('Escape sequence were handled incorrectly')
-
-    ESRT_COUNTER -= 1
-
-
-def preprocess_slice_message(msg: str) -> list[tuple[str, int]]:
-    msg_layout: list[tuple[str, int]] = []
-
-    for char in msg:
-        for idx in range(1, get_symbol(escape_char(char)).width + 1):
-            msg_layout.append((char, idx))
-
-        msg_layout.append((' ', 1))
-
-    return msg_layout
-
-
-def main(conn: max7219cng):
-    msg = f'   {input("Your Message: ")}   '
-    msg = preprocess_escape_sequences(msg)
-    layout = preprocess_slice_message(msg)
-    for msg_line in range(len(layout) - 8):
-        for matrix_line in range(8):
-            c_l_tuple = layout[msg_line + matrix_line]
-
-            char = escape_char(c_l_tuple[0])
-            conn.write(get_symbol_line(char, c_l_tuple[1], matrix_line + 1))
-        sleep(0.1)
-
-    for i in range(ESRT_COUNTER - 1, PUA_LOWER, -1):
-        remove_rename(i)
-
-
-def test(conn: max7219cng):
-    conn.test(3)
+from lib import print2matrix
 
 
 if __name__ == '__main__':
     try:
         matrix = max7219cng(clk=16, cs=20, mosi=21)
 
-        r = 0
+        mode = 0
         while True:
-            r = int(input("1: Run)  2: Test) 3: Quit)\n> "), 10)
-
-            if r == 1:
-                main(matrix)
+            try:
+                mode = int(input('1: Run) 2: Contiguous) 3: Test) 4: Quit)\n> ')) # noqa
+            except ValueError:
+                continue
+            if mode == 1:
+                print2matrix(input('Your message: '), matrix)
                 break
-            elif r == 2:
-                test(matrix)
+            elif mode == 2:
+                msg = input('Your message: ')
+                print('Ctrl + C to stop')
+                while True:
+                    print2matrix(msg, matrix, lpadding=2, rpadding=2)
+            elif mode == 3:
+                matrix.test(3)
                 break
-            elif r == 3:
+            elif mode == 4:
                 break
 
     except KeyboardInterrupt:
